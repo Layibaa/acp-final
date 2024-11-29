1cc9ff4f8b2d051b08c99e5c8b7de0ed4a66cf58
Layiba <layibakafayat@gmail.com>
2024-11-29 20:28:27 +0500

    Home page layout
    made home page divided it into 3 parts left mid and right
    also started making components

---
+++ starter/src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const profileSide = () => {
+  return (
+    <div>
+      <div className="ProfileSide">
+        ProfileSide
+      </div>
+    </div>
+  )
+}
+
+export default profileSide
