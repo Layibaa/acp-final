3e540be0b838c0e9e0ad3fc3fc65364b0d489b29
Layiba <layibakafayat@gmail.com>
2024-11-30 23:13:10 +0500

    half done profile card (left side home)

--- starter/src/components/profileSide/ProfileSide.jsx
+++ starter/src/components/profileSide/ProfileSide.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
+import ProfileCard from '../ProfileCard/ProfileCard'
+
 const profileSide = () => {
   return (
     <div>
       <div className="ProfileSide">
         <LogoSearch/>
+        <ProfileCard/>
       </div>
     </div>
   )
