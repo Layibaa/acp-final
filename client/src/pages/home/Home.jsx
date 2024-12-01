e1c59186c90859aa5fcdb88ced25dbf07b571d9b
Layiba <layibakafayat@gmail.com>
2024-12-02 04:22:09 +0500

    profile card setup

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import PostSide from "../../components/PostSide/PostSide";
-import ProfileSide from "../../components/ProfileSide/ProfileSide";
-import RightSide from "../../components/RightSide/RightSide";
+import PostSide from "../components/PostSide/PostSide";
+import ProfileSide from "../components/profileSide/ProfileSide";
+import RightSide from "../components/RightSide/RightSide";
 import "./Home.css";
-
 const Home = () => {
   return (
     <div className="Home">
