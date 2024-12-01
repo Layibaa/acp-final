fb9b7ebef5a6cb3ac4dd69e8d2d38a197a254e68
Layiba <layibakafayat@gmail.com>
2024-12-01 16:19:10 +0500

    post share done

--- starter/src/components/FollowersCard/FollowersCard.jsx
+++ starter/src/components/FollowersCard/FollowersCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './FollowersCard.css'
 
-import Followers  from '../../Data/FollowersData'
+import { Followers } from '../../Data/FollowersData'
 const FollowersCard = () => {
   return (
     <div className="FollowersCard">
         <h3>Who is following you</h3>
 
-        {Followers. map((follower, id)=>{
+        {Followers.map((follower, id)=>{
             return(
                 <div className="follower">
                     <div>
--- starter/src/pages/home/Home.jsx
+++ starter/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
+import PostSide from '../../components/PostSide/PostSide'
 import ProfileSide from '../../components/ProfileSide/ProfileSide'
 const home = () => {
   return (
     <div className="Home">
       <ProfileSide/>
+      <PostSide/>
       <div className="postSide">Posts</div>
       <div className="rightSide">Rightside</div>
     </div>
