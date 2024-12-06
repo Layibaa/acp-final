7140a73b6d7a878410d788ff1f017db908379241
Layiba <layibakafayat@gmail.com>
2024-12-06 14:53:39 +0500

    final

--- client/src/actions/AuthActions.js
+++
@@ -1,4 +0,0 @@
-// No authentication actions needed anymore.
-export const logout = () => (dispatch) => {
-  dispatch({ type: "LOG_OUT" });
-};
--- server/Routes/UserRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-// No authentication routes anymore
-const router = express.Router();
-
-// You can add other user-related routes here (without login/signup logic).
-
-export default router;
